23cd155d4dbe5306feeb6ce2074cfd06117e77a8
sakameow.github.io <getzynokos@gmail.com>
2025-02-23 08:44:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const products = [
     { name: "Panda Vpn", price: "Rp10.000", image: "images/panda.jpg", description: "7 Day Premium, Private" },
     { name: "Pia Vpn", price: "Rp10.000", image: "images/pia.jpg", description: "7 Day Premium, Private" },
     { name: "Nord Vpn", price: "Rp10.000", image: "images/nord.jpg", description: "7 Day Premium, Private" },
-    { name: "Capcut Pro", price: "Rp25.000", image: "images/capcut.jpg", description: "Capcut Pro 1 Bulan" },
+    { name: "Capcut Pro", price: "Rp31.000", image: "images/capcut.jpg", description: "Capcut Pro 1 Bulan" },
     { name: "Alight Motion Premium", price: "Rp8.000-Rp20.000", image: "images/am.jpg", description: "1 Tahun Premium Sharing-Private" },
     { name: "Netflix Premium", price: "Rp31.000-Rp40.000", image: "images/netflix.jpg", description: "Netflix Premium 1 Bulan 1P2U / 1 Bulan 1P1U HD4K" },
     { name: "Suntik Like Ig & Tiktok", price: "Rp2.000-Rp16.000", image: "images/suntik.jpg", description: "Suntik Like Tiktok & Ig Fast Proses" },
