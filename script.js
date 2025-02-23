4d5fc362878003455471df7c17f3e4322a4f4d7b
sakameow.github.io <getzynokos@gmail.com>
2025-02-23 08:47:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const products = [
-    { name: "Surfshark Vpn", price: "Rp10.000", image: "images/surfshark.jpg", description: "7 Day Premium, Private" },
+    { name: "Room Wangi Mlbb", price: "Rp30.000-Rp50.000", image: "images/rw.jpg", description: "Free vpn rw indo + Luar 15+ Tutor" },
     { name: "Express Vpn", price: "Rp10.000", image: "images/express.jpg", description: "7 Day Premium, Private" },
-    { name: "Panda Vpn", price: "Rp10.000", image: "images/panda.jpg", description: "7 Day Premium, Private" },
+    { name: "Surfshark", price: "Rp10.000", image: "images/surfshark.jpg", description: "7 Day Premium, Private" },
     { name: "Pia Vpn", price: "Rp10.000", image: "images/pia.jpg", description: "7 Day Premium, Private" },
     { name: "Nord Vpn", price: "Rp10.000", image: "images/nord.jpg", description: "7 Day Premium, Private" },
     { name: "Capcut Pro", price: "Rp31.000", image: "images/capcut.jpg", description: "Capcut Pro 1 Bulan" },
